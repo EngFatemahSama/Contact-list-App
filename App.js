19ad3d3b877799758dfde6ac86445f53e28bb483
EngFatemahSama <samafatemah@gmail.com>
2021-02-09 14:32:05 +0430

    contact-listscreens_navigation  and database

--- App.js
+++ App.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-import Profile from './screen/profile'
-
+import { StatusBar } from 'expo-status-bar';
+import React,{useEffect} from 'react';  
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons'  
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as SQLite from "expo-sqlite";  
+const Stack = createStackNavigator();
+const db = SQLite.openDatabase('mobileApp.db');     
+import Contacts from "./screens/Contacts";
+import Profile from "./screens/profile";
+import CreateContacts from "./screens/createContacts";     
+import SearchScreen from './screens/searchScreen';
 export default function App() {
+  useEffect(()=>{
+     db.transaction(tx=>{
+       tx.executeSql('create table mobileApp(id integer primary key autoincrement, name text ,phone text,email text)')
+     },[],()=>console.log('table created successfully')); 
+  })
   return (
     <View style={styles.container}>
-      <Profile  name="FatemahSama"  phone="0799878790" email="fatemah@gmail.com"/>
+      <NavigationContainer>
+        <Stack.Navigator>
+           <Stack.Screen name="Contacts" component={Contacts}
+            options={({navigation})=>({
+              headerRight:()=>(   
+                <TouchableOpacity style={{paddingRight:20}} onPress={()=> navigation.navigate('Sreach')}>
+                  <Text>
+                    <MaterialIcons name="search" size={24} color="black"/>
+                  </Text>   
+                </TouchableOpacity>
+              )
+            })} />
+           <Stack.Screen name="CreateContact" component={CreateContacts} />
+           <Stack.Screen name="Profile" component={Profile} />
+           <Stack.Screen name="Sreach" component={SearchScreen} />
+        </Stack.Navigator>   
+      </NavigationContainer>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
+  container: {   
+    flex: 1,
+    backgroundColor:"#f8f4f4"   
   },
 });
---
+++ screens/profile.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+import Colors from '../utils/colors';
+import ContactThumbnail from '../components/contactThumbnail';
+import DetailListItem from '../components/DetailListItem';
+export default function Profile(props){
+    const {route} = props
+    const {item} = route.params
+    const {name,phone,email}=item
+    return (
+        <View style={styles.container} >
+            <View style={styles.thumbnailSection}>
+                <ContactThumbnail name={name} phone={phone}/>
+            </View>
+            <View style={styles.detailSection}>
+                <DetailListItem icon="email" title="Email" subTitle={email} />
+                <DetailListItem icon="phone" title="Work" subTitle={phone} />
+            </View>
+        </View>
+
+    )
+
+}
+const styles =StyleSheet.create({
+    container:{
+        flex:1
+    },
+    thumbnailSection:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:Colors.primary,
+        borderBottomWidth:1,
+        borderBottomColor:Colors.secondary
+    },
+    detailSection:{
+        flex:1,
+        paddingTop:10
+    }
+})
